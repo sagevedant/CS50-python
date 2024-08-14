b78bc67c7c6bcec86a7415f1685c520c94fdbf85
sagevedant <bhavevedant18@gmail.com>
2024-08-14 12:00:51 +0000

    container config

---
+++ 02_Loops/loops.py
@@ -0,0 +1,15 @@
+# LOOPS --> The block of code that is going to repeat code for as many times as we want.
+#1. WHILE LOOP
+ # break  Use BREAK to terminate the while loop started if the condition is met.
+
+mobNo = input("Please enter your mobile No. ")
+
+while len(mobNo) != 10 :
+   print("https")
+    
+else:
+    print("You have entered correct mobile number")
+
+#-----------------+++-----------------------
+
+#2. FOR LOOP
